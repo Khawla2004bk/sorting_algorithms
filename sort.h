ee7e1b0bad29775ac44b56be8f31b704a6524dc2
khawla <boukniter.khawla@gmail.com>
2023-07-23 07:12:06 -0700

    0. Bubble sort

--- sort.h
+++ sort.h
@@ -17,40 +17,6 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/**
- * swapint - Swap two integers in an array
- * @i: first integer to swap
- * @j: second integer to swap
- */
-void swapint(int *i, int *j)
-{
-	int temp;
-
-	temp = *i;
-	*i = *j;
-	*j = temp;
-}
-
-/**
- * swapnode - Swap two nodes in listint_t doubly linked list
- * @head: pointer to the head of the doubly linked list
- * @i: pointer to the first node to swap
- * @j: second node to swap
- */
-void swapnode(listint_t **head, listint_t **i, listint_t *j)
-{
-	(*i)->next = j->next;
-	if (j->next != NULL)
-		j->next->prev = *i;
-	j->prev = (*i)->prev;
-	j->next = *i;
-	if ((*i)->prev != NULL)
-		(*i)->prev->next = j;
-	else
-		*head = j;
-	(*i)->prev = j;
-	*i = j->prev;
-}
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
@@ -60,7 +26,5 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-void swapnode(listint_t **head, listint_t **i, listint_t *j);
-void swapint(int *i, int *j);
 
 #endif
